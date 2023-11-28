ffcfbf57f6690048912e954a141cc1b689f0f39d
TimoteDonini <142145405+TimoteDonini@users.noreply.github.com>
2023-11-29 06:25:58 +0800

    update

--- client/src/pages/PowerBi.tsx
+++ client/src/pages/PowerBi.tsx
@@ -144,6 +144,7 @@ const PowerBiContent: React.FC = () => {
             var wb = XLSX.utils.book_new();
             for (var i = 0; i < visulas.length; i++) {
                 const response = await visulas[i].exportData(powerbi.models.ExportDataType.Summarized, 1000)
+                console.log(response.data)
                 var ws = XLSX.read(response.data, { type: 'string' });
                 var sheetName = ws.SheetNames[0];
                 var sheet = ws.Sheets[sheetName];
